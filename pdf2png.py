b3fc3411581b16a55f3df43414a6090cad82ccb9
Wang Guo <31676811+King-Key@users.noreply.github.com>
2023-07-13 15:07:59 +0800

    Update pdf2png.py

--- pdf2png.py
+++ pdf2png.py
@@ -14,39 +14,39 @@ def save_uploaded_file(uploadedfile):
 def pdf2png():
     html("<center><h1> pdf 转 png </center>")
 
-datafile = st.file_uploader("Upload pdf",type=['pdf'])
+    datafile = st.file_uploader("Upload pdf",type=['pdf'])
 
 
-if datafile is not None:
-    file_details = {"FileName":datafile.name,"FileType":datafile.type}
-    # df  = pd.read_csv(datafile)
-    # st.dataframe(df)
-    # Apply Function here
-    save_uploaded_file(datafile)
-
-    print(datafile)
-
-
-    doc=fitz.open("./"+datafile.name)
-    pages=doc.page_count
-    st.write("文件页数：",pages)
-
-
-    for page in stqdm(range(pages)):
-      page1=doc.load_page(page)
-      png=page1.get_pixmap(alpha=True)
+    if datafile is not None:
+        file_details = {"FileName":datafile.name,"FileType":datafile.type}
+        # df  = pd.read_csv(datafile)
+        # st.dataframe(df)
+        # Apply Function here
+        save_uploaded_file(datafile)
     
-      if os.path.exists(output) == False:
-        os.mkdir(output)
+        print(datafile)
+    
+    
+        doc=fitz.open("./"+datafile.name)
+        pages=doc.page_count
+        st.write("文件页数：",pages)
+    
+    
+        for page in stqdm(range(pages)):
+          page1=doc.load_page(page)
+          png=page1.get_pixmap(alpha=True)
         
-
-      png.save( output +"{}.png".format(page))
+          if os.path.exists(output) == False:
+            os.mkdir(output)
+            
     
-    st.write("转换完成！！！")
-    st.balloons()
-
-    for i in range(fitz.open("./"+datafile.name).page_count):
-      image=os.path.join(output+"{}.png".format(i))
-
-      # image
-      st.image(Image.open(image))
+          png.save( output +"{}.png".format(page))
+        
+        st.write("转换完成！！！")
+        st.balloons()
+    
+        for i in range(fitz.open("./"+datafile.name).page_count):
+          image=os.path.join(output+"{}.png".format(i))
+    
+          # image
+          st.image(Image.open(image))
