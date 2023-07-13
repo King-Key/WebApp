d96639f0407a82bc92a1b9572c2648ee92ac4fcf
Wang Guo <31676811+King-Key@users.noreply.github.com>
2023-07-13 15:05:54 +0800

    Update pdf2png.py

--- pdf2png.py
+++ pdf2png.py
@@ -13,39 +13,40 @@ def save_uploaded_file(uploadedfile):
 
 def pdf2png():
     html("<center><h1> pdf 转 png </center>")
-    output="./"
-    datafile = st.file_uploader("Upload pdf",type=['pdf'])
 
-    if datafile is not None:
-        file_details = {"FileName":datafile.name,"FileType":datafile.type}
-        # df  = pd.read_csv(datafile)
-        # st.dataframe(df)
-        # Apply Function here
-        save_uploaded_file(datafile)
-
-        print(datafile)
+datafile = st.file_uploader("Upload pdf",type=['pdf'])
 
 
-        doc=fitz.open("./"+datafile.name)
-        pages=doc.page_count
-        st.write("文件页数：",pages)
+if datafile is not None:
+    file_details = {"FileName":datafile.name,"FileType":datafile.type}
+    # df  = pd.read_csv(datafile)
+    # st.dataframe(df)
+    # Apply Function here
+    save_uploaded_file(datafile)
+
+    print(datafile)
 
 
-        for page in stqdm(range(pages)):
-            page1=doc.load_page(page)
-            png=page1.get_pixmap(alpha=True)
+    doc=fitz.open("./static/"+datafile.name)
+    pages=doc.page_count
+    st.write("文件页数：",pages)
+
+
+    for page in stqdm(range(pages)):
+      page1=doc.load_page(page)
+      png=page1.get_pixmap(alpha=True)
+    
+      if os.path.exists(output) == False:
+        os.mkdir(output)
         
-        if os.path.exists(output) == False:
-            os.mkdir(output)
-            
 
-        png.save( output +"{}.png".format(page))
-        
-        st.write("转换完成！！！")
-        st.balloons()
+      png.save( output +"{}.png".format(page))
+    
+    st.write("转换完成！！！")
+    st.balloons()
 
-        for i in range(fitz.open("./"+datafile.name).page_count):
-            image=os.path.join(output+"{}.png".format(i))
+    for i in range(fitz.open("./static/"+datafile.name).page_count):
+      image=os.path.join(output+"{}.png".format(i))
 
-        # image
-        st.image(Image.open(image))
+      # image
+      st.image(Image.open(image))
