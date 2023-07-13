ee86fab3b5528cb0e057edfc98a55d3aa3218ec1
Wang Guo <31676811+King-Key@users.noreply.github.com>
2023-07-13 14:59:17 +0800

    Add files via upload

---
+++ mainUI.py
@@ -0,0 +1,19 @@
+import streamlit as st 
+from pdf2png import pdf2png
+from search_github_app import search_github
+from search_arxiv_app import search_arxiv
+
+st.set_page_config(page_title='Streamlit App')
+
+app_pages = {
+    'pdf2png': pdf2png,
+    'GitHub': search_github,
+    'Arxiv': search_arxiv
+}
+
+st.sidebar.write("Web APP")
+st.sidebar.write("---")
+page = st.sidebar.radio('选择页面', tuple(app_pages.keys()))
+
+# 执行选择的页面
+app_pages[page]()
---
+++ pdf2png.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import os 
+import fitz
+from PIL import Image
+import time
+from stqdm import stqdm
+from streamlit.components.v1 import html 
+
+def save_uploaded_file(uploadedfile):
+  with open(os.path.join("./",uploadedfile.name),"wb") as f:
+     f.write(uploadedfile.getbuffer())
+  return st.success("Saved file :{} ".format(uploadedfile.name))
+
+def pdf2png():
+    html("<center><h1> pdf 转 png </center>")
+    output="./"
+    datafile = st.file_uploader("Upload pdf",type=['pdf'])
+
+    if datafile is not None:
+        file_details = {"FileName":datafile.name,"FileType":datafile.type}
+        # df  = pd.read_csv(datafile)
+        # st.dataframe(df)
+        # Apply Function here
+        save_uploaded_file(datafile)
+
+        print(datafile)
+
+
+        doc=fitz.open("./"+datafile.name)
+        pages=doc.page_count
+        st.write("文件页数：",pages)
+
+
+        for page in stqdm(range(pages)):
+            page1=doc.load_page(page)
+            png=page1.get_pixmap(alpha=True)
+        
+        if os.path.exists(output) == False:
+            os.mkdir(output)
+            
+
+        png.save( output +"{}.png".format(page))
+        
+        st.write("转换完成！！！")
+        st.balloons()
+
+        for i in range(fitz.open("./"+datafile.name).page_count):
+            image=os.path.join(output+"{}.png".format(i))
+
+        # image
+        st.image(Image.open(image))
